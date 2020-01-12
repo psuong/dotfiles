8243227eccbff1104efde051ad98c55fce9d31a9
Porrith Suong <porrith.suong@gmail.com>
2020-01-12 18:49:13 -0500

    Updates and adds the install script.

--- Unix/install.sh
+++ Unix/install.sh
@@ -4,12 +4,12 @@ cwd=$(pwd)
 
 if [ -e $HOME/.bashrc ]
 then
-	mv $HOME/.bashrc .bashrc.orig
+    mv $HOME/.bashrc $HOME/.bashrc.orig
 fi
 
 if [ -e $HOME/.bash_profile ]
 then
-	mv $HOME/.bash_profile .bash_profile.orig
+    mv $HOME/.bash_profile $HOME/.bash_profile.orig
 fi
 
 cd $HOME
@@ -18,17 +18,17 @@ ln -s $cwd/.bash_profile .bash_profile
 
 if [ -d .config ]
 then
-	echo "dir exists"
-	if [ -d .config/nvim ] 
-	then
-		mv .config/nvim .config/nvim.orig/
-	fi
-	ln -s $cwd/.config/* $HOME/.config
+    echo "dir exists"
+    if [ -d .config/nvim ] 
+    then
+        mv .config/nvim .config/nvim.orig/
+    fi
+    ln -s $cwd/.config/* $HOME/.config
 else
-	echo else
-	ln -s $cwd/.config/ $HOME/.config/
+    echo else
+    ln -s $cwd/.config/ $HOME/.config/
 fi
-#ln -s $cwd/.vim $HOME/.vim
-#ln -s $cwd/.git-prompt.sh $HOME/.git-prompt.sh
+ln -s $cwd/.vim $HOME/.vim
+ln -s $cwd/.git-prompt.sh $HOME/.git-prompt.sh
 
 cd $cwd
