45080a52a23aef036dc0aeac17e51e3519b64f0d
Porrith Suong <porrith.suong@gmail.com>
2020-01-12 12:56:11 -0500

    reorg for install script.

---
+++ Unix/install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+cwd=$(pwd)
+
+if [ -e $HOME/.bashrc ]
+then
+	mv $HOME/.bashrc .bashrc.orig
+fi
+
+if [ -e $HOME/.bash_profile ]
+then
+	mv $HOME/.bash_profile .bash_profile.orig
+fi
+
+cd $HOME
+
+ln -s $cwd/.bash_profile .bash_profile
+
+if [ -d .config ]
+then
+	echo "dir exists"
+	if [ -d .config/nvim ] 
+	then
+		mv .config/nvim .config/nvim.orig/
+	fi
+	ln -s $cwd/.config/* $HOME/.config
+else
+	echo else
+	ln -s $cwd/.config/ $HOME/.config/
+fi
+#ln -s $cwd/.vim $HOME/.vim
+#ln -s $cwd/.git-prompt.sh $HOME/.git-prompt.sh
+
+cd $cwd
