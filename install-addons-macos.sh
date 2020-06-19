49831b90d4e4bf1c3072383105522d4e6adf66fb
Porrith Suong <porrith.suong@gmail.com>
2020-06-19 12:09:29 -0400

    Updates the install-addons-macos shell script.

--- install-addons-macos.sh
+++ install-addons-macos.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
-VULKAN_SDK_ROOT="$HOME/sources/vulkan-macos-"
-VULKAN_SDK_VERSION="1.2.135.0"
-
 if [ ! -f ~/.bash_aliases ]; then
     echo ".bash_aliases is not found, creating one at the home directory"
     touch ~/.bash_aliases
 fi
 
 # Bash Warning
-echo "export BASH_SILENCE_DEPRECATION_WARNING=1"
+echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_aliases
 
 # Git completion
-echo "source ~/.git-prompt.sh"
-echo "source ~/.git-completion.bash"
+echo "source ~/.git-prompt.sh" >> ~/.bash_aliases
+echo "source ~/.git-completion.bash" >> ~/.bash_aliases
 
 # Vulkan support
-echo "export VULKAN_SDK="$HOME/sources/$VULKAN_SDK_ROOT$VULKAN_SDK_VERSION/macOS"" >> ~/.bash_aliases
-echo "export VK_ICD_FILENAMES="$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json"" >> ~/.bash_aliases
-echo "export VK_LAYER_PATH="$VULKAN_SDK/etc/vulkan/explicit_layers.d"" >> ~/.bash_aliases
-echo "export PATH="$VULKAN_SDK:$PATH"" >> ~/.bash_aliases
+VULKAN_SDK_ROOT="$HOME/sources/vulkansdk-macos-"
+VULKAN_SDK_VERSION="1.2.135.0"
+VULKAN_ROOT="$VULKAN_SDK_ROOT$VULKAN_SDK_VERSION"
+echo "source $VULKAN_ROOT/setup-env.sh" >> ~/.bash_aliases
+
+# echo "export VULKAN_DIR="$HOME/sources/$VULKAN_SDK_ROOT$VULKAN_SDK_VERSION/macOS"" >> ~/.bash_aliases
+# echo "export VK_ICD_FILENAMES="\$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json"" >> ~/.bash_aliases
+# echo "export VK_LAYER_PATH="\$VULKAN_SDK/etc/vulkan/explicit_layers.d"" >> ~/.bash_aliases
+# echo "export DYLD_LIBRARY_PATH="\$VULKAN_SDK/lib/"" >> ~/.bash_aliases
+# echo "export PATH="\$VULKAN_SDK:$PATH"" >> ~/.bash_aliases
 
 if [ ! -f ~/.bashrc ]; then
     echo "Reloading the .bashrc file"
