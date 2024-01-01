4134d017f17c8bf4babc3dfc56a78db17f8321c3
Porrith Suong <porrith.suong@gmail.com>
2024-01-01 15:07:10 -0500

    Updates powershell install instructions

--- install.ps1
+++ install.ps1
@@ -1,3 +1,8 @@
+New-Item -ItemType SymbolicLink -Target C:\Users\porri\sources\dotfiles\PowerShell\ -Path C:\Users\porri\Documents\PowerShell
+
+winget install JanDeDobbeleer.OhMyPosh -s winget
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+
 New-Item -ItemType SymbolicLink -Path $HOME\.vim -Target $HOME\sources\dotfiles\.vim
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Local\nvim -Target $HOME\sources\dotfiles\.config\nvim
 New-Item -ItemType SymbolicLink -Path $HOME\AppData\Roaming\alacritty -Target $HOME\sources\dotfiles\.config\alacritty
