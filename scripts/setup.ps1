791a3619025c82ccc5aa075c4eb7798bbf31c81f
Porrith Suong <porrith.suong@gmail.com>
2024-08-20 11:34:41 -0400

    Adds vim-clap configs.

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,9 +1,10 @@
-New-Item -ItemType SymbolicLink -Target "$HOME\\sources\\dotfiles\\PowerShell" -Path "C:\\Users\\porri\\Documents\\PowerShell"
-
-winget install JanDeDobbeleer.OhMyPosh -s winget
-PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
-PowerShellGet\Install-Module PSCompletions -Scope CurrentUser -Force
-
-New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$HOME\sources\dotfiles\.config\nvim"
-New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Roaming\alacritty" -Target "$HOME\sources\dotfiles\.config\alacritty"
+# New-Item -ItemType SymbolicLink -Target "$HOME\\sources\\dotfiles\\PowerShell" -Path "C:\\Users\\porri\\Documents\\PowerShell"
+# 
+# winget install JanDeDobbeleer.OhMyPosh -s winget
+# PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+# PowerShellGet\Install-Module PSCompletions -Scope CurrentUser -Force
+# 
+# New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim" -Target "$HOME\sources\dotfiles\.config\nvim"
+# New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Roaming\alacritty" -Target "$HOME\sources\dotfiles\.config\alacritty"
+New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Roaming\Vim\Vim Clap\config\config.toml" -Target "$HOME\sources\dotfiles\.config\vim-clap\config.toml"
 
