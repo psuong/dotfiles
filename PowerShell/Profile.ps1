74510d057befa313c0e8b50a1c3b6f7d627520b9
Porrith Suong <porrith.suong@gmail.com>
2024-08-26 23:13:23 -0400

    Adds tere

--- PowerShell/Profile.ps1
+++ PowerShell/Profile.ps1
@@ -3,3 +3,11 @@ oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\negligible.omp.json | In
 
 Import-Module Posh-Git
 Import-Module PSCompletions
+
+function Invoke-Tere() {
+    $result = . (Get-Command -CommandType Application tere) $args
+    if ($result) {
+        Set-Location $result
+    }
+}
+Set-Alias tere Invoke-Tere
