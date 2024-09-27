8ea671a159d42099afe566458f467a1aefba597d
Porrith Suong <porrith.suong@gmail.com>
2024-09-27 08:46:23 -0400

    Removes PsCompletions.

--- PowerShell/Profile.ps1
+++ PowerShell/Profile.ps1
@@ -2,7 +2,9 @@
 oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\negligible.omp.json | Invoke-Expression
 
 Import-Module Posh-Git
-Import-Module PSCompletions
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -PredictionViewStyle List
 
 function Invoke-Tere() {
     $result = . (Get-Command -CommandType Application tere) $args
