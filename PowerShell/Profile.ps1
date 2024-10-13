7c45a639f84ac1853022dcfc03f6aab593f10980
Porrith Suong <porrith.suong@gmail.com>
2024-10-12 20:23:14 -0400

    Adds terminal consistent sizing when toggling.

--- PowerShell/Profile.ps1
+++ PowerShell/Profile.ps1
@@ -1,17 +1,18 @@
 # Theme
-oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH\negligible.omp.json | Invoke-Expression
+oh-my-posh.exe init pwsh --config "$env:POSH_THEMES_PATH\negligible.omp.json" | Invoke-Expression
 
-if (-not (Get-Module -Name Posh-Git)) {
-    Import-Module Posh-Git
+$modules = @('Posh-Git', 'PSReadLine')
+foreach ($module in $modules) {
+    if (-not (Get-Module -Name $module -ListAvailable)) {
+        Import-Module $module
+    }
 }
-if (-not (Get-Module -Name PSReadLine)) {
-    Import-Module PSReadLine
-}
+
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -PredictionViewStyle List
 
-function Invoke-Tere() {
-    $result = . (Get-Command -CommandType Application tere) $args
+function Invoke-Tere {
+    $result = & (Get-Command -CommandType Application tere) $args
     if ($result) {
         Set-Location $result
     }
