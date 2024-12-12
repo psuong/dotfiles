440aa7a8f9f499975eb4a84a3e7b26a6069c8817
Porrith Suong <porrith.suong@gmail.com>
2024-12-11 23:03:41 -0500

    Updates configs for diagnostic navigation.

--- .config/nvim/configs/editor.lua
+++ .config/nvim/configs/editor.lua
@@ -279,10 +279,8 @@ local function common_keybindings()
     local_map("n", "<Leader>nm", vim.lsp.buf.rename, "Rename symbol");
     local_map("n", "<Leader>cf", vim.lsp.buf.format, "Run code formatting");
     local_map("n", "<Leader>pd", vim.lsp.buf.hover, "Preview info above cursor");
-    local_map("n", "[[", vim.diagnostic.goto_prev, "Go to previous diagnostic")
-    local_map("n", "]]", vim.diagnostic.goto_prev, "Go to previous diagnostic")
-    -- vim.api.nvim_set_keymap("n", "[[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true });
-    -- vim.api.nvim_set_keymap("n", "]]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true });
+    vim.keymap.set("n", ".", vim.diagnostic.goto_next, { noremap = true, silent = true })
+    vim.keymap.set("n", ",", vim.diagnostic.goto_prev, { noremap = true, silent = true })
     local_map("n", "<Leader>e", function()
         vim.diagnostic.open_float(nil, { focus = false })
     end, 'Show diagnostics float')
