d3fa75a39f5451b2838174268b50f248aaa82eb5
Porrith Suong <porrith.suong@gmail.com>
2025-12-03 11:42:37 -0500

    Adds broot, removes Alt Enter full screen.

--- PowerShell/Profile.ps1
+++ PowerShell/Profile.ps1
@@ -22,3 +22,24 @@ function Invoke-Tere {
     }
 }
 Set-Alias tere Invoke-Tere
+
+Function br {
+    $args = $args -join ' '
+    $cmd_file = New-TemporaryFile
+
+    $process = Start-Process -FilePath 'broot.exe' `
+        -ArgumentList "--outcmd $($cmd_file.FullName) $args" `
+        -NoNewWindow -PassThru -WorkingDirectory $PWD
+
+    Wait-Process -InputObject $process #Faster than Start-Process -Wait
+    If ($process.ExitCode -eq 0) {
+        $cmd = Get-Content $cmd_file
+        Remove-Item $cmd_file
+        If ($null -ne $cmd) { Invoke-Expression -Command $cmd }
+    }
+    Else {
+        Remove-Item $cmd_file
+        Write-Host "`n" # Newline to tidy up broot unexpected termination
+        Write-Error "broot.exe exited with error code $($process.ExitCode)"
+    }
+}
