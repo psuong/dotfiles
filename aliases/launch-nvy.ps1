9987b2e4fe3eae4735f5b2b6c877b49839499535
Porrith Suong <porrith.suong@gmail.com>
2026-07-17 13:02:28 -0400

    Updates launch-nvy.ps1

--- aliases/launch-nvy.ps1
+++ aliases/launch-nvy.ps1
@@ -1,1 +1,6 @@
-nvy.exe --maximize
+if (Get-Command "nvy.exe" -ErrorAction SilentlyContinue) {
+    Invoke-Expression "nvy.exe --maximize --disable-fullscreen"
+}
+else {
+    Write-Error "nvy.exe not found in PATH" -ForegroundColor Red
+}
--- aliases/nav.ps1
+++
@@ -1,1 +0,0 @@
-tere -m "tab:CursorDown,ctrl-j:CursorDown,ctrl-k:CursorUp,ctrl-h:CursorDownScreen,ctrl-l:CursorUpScreen"
